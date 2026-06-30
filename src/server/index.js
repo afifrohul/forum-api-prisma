d51e51b3cc20696d7379304034134323d9cd38b8
afifrohul <afifmemyself22@gmail.com>
2026-06-30 13:02:41 +0700

    fix: remove swagger

--- src/server/index.js
+++ src/server/index.js
@@ -2,45 +2,10 @@ import express from "express";
 import cors from "cors";
 import routes from "../routes/index.js";
 import ErrorHandler from "../middlewares/error.js";
-import swaggerJsDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-
-const port = process.env.PORT;
-const host = process.env.HOST;
-const hostv1 = process.env.HOSTV1;
-
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Forum API Prisma Documentation",
-      version: "1.0.0",
-      description: "API Documentation with Swagger + Express + Prisma ORM",
-    },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
-        },
-      },
-    },
-    servers: [{ url: `${host}:${port}/v1` }, { url: `${hostv1}` }],
-  },
-  apis: ["./src/services/users/routes/index.js"],
-};
-const openapiSpecification = swaggerJsDoc(swaggerOptions);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/test", (req, res) => {
-  res.send("OK");
-});
-app.use("/v1/api-docs", swaggerUi.serve);
-
-app.get("/v1/api-docs", swaggerUi.setup(openapiSpecification));
 app.use(routes);
 app.use(ErrorHandler);
 
